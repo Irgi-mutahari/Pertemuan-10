b6de921fe3e4c0c6cb62ad32d16de24c4ef1fa7f
Irgi-mutahari <115854644+Irgi-mutahari@users.noreply.github.com>
2022-12-21 20:01:43 +0700

    Create switchcase.java

---
+++ switchcase.java
@@ -0,0 +1,29 @@
+public class Switchcase {
+	public static void main(String[] args) {
+		int day = 3;
+		switch (day) {
+		case 1:
+			System.out.println("Senin");
+			break;
+		case 2:
+			System.out.println("Selasa");
+			break;
+		case 3:
+			System.out.println("rabu");
+			break;
+		case 4:
+			System.out.println("Kamis");
+			break;
+		case 5:
+			System.out.println("Jumat");
+			break;
+		case 6:
+			System.out.println("Sabtu");
+			break;
+		case 7:
+			System.out.println("Minggu");
+			break;
+		}
+	}
+
+}
